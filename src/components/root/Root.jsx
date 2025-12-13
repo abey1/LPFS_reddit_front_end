3d0d68f6463453a08fafb5d8acd66237beecfd52
abey <bruckabey@gmail.com>
2025-12-14 08:12:53 +1030

    sidebar responsive design impleimentation finished. next use redux to toggle the opening and closing of the sidebar

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -8,10 +8,10 @@ const Root = () => {
       <Nav />
 
       <div className="flex flex-1">
-        <main className="flex-1 p-4 overflow-y-auto  border border-red-500 mt-20 lg:mr-66 md:mr-70">
+        <main className="flex-1 p-4 overflow-y-auto  mt-20 lg:mr-66 md:mr-45 ">
           <Outlet />
         </main>
-        <aside className="w-64 bg-gray-200 p-4 fixed  top-20 h-full right-30 lg:block md:block hidden">
+        <aside className="w-64 bg-gray-200 p-4 fixed  top-20 h-full lg:right-10 md:right-0 right-0 lg:block md:block hidden">
           <SideBarSubReddit />
         </aside>
       </div>
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { LiaSearchSolid } from "react-icons/lia";
 import { useNavigate } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
 const Search = () => {
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
@@ -13,23 +15,28 @@ const Search = () => {
     navigate(`/search/${data.search}`);
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex gap-3 justify-center items-center">
-          <label htmlFor="search">
-            <LiaSearchSolid className="text-3xl" />
-          </label>
+    <div className="flex justify-between w-full">
+      <div className="w-full">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex gap-3 justify-center items-center">
+            <label htmlFor="search">
+              <LiaSearchSolid className="text-3xl" />
+            </label>
 
-          <input
-            id="search"
-            name="search"
-            type="text"
-            placeholder="search reddit"
-            className="border rounded-2xl px-3 py-2"
-            {...register("search")}
-          />
-        </div>
-      </form>
+            <input
+              id="search"
+              name="search"
+              type="text"
+              placeholder="search reddit"
+              className="border rounded-2xl px-3 py-2 lg:w-140 md:w-100 sm:w-80 xm:w-60 w-40"
+              {...register("search")}
+            />
+          </div>
+        </form>
+      </div>
+      <div className="lg:hidden md:hidden block flex items-center justify-center ml-2">
+        <FaBars />
+      </div>
     </div>
   );
 };

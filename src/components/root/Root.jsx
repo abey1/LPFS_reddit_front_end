0eb3ff039835a3d34f8e9d92022c4422a9e8e268
abey <bruckabey@gmail.com>
2025-12-13 21:43:57 +1030

    i got the nav bar on the possible best position and the side bar is sticky too and works fine. the page is made responsive not the side bar poping out yet though

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "../nav/Nav.jsx";
+import SideBarSubReddit from "../side_bar_subreddit/SideBarSubReddit.jsx";
 const Root = () => {
   return (
     <>
       <Nav />
-      <Outlet />
+
+      <div className="flex flex-1">
+        <main className="flex-1 p-4 overflow-y-auto  border border-red-500 mt-20 lg:mr-66 md:mr-70">
+          <Outlet />
+        </main>
+        <aside className="w-64 bg-gray-200 p-4 fixed  top-20 h-full right-30 lg:block md:block hidden">
+          <SideBarSubReddit />
+        </aside>
+      </div>
     </>
   );
 };

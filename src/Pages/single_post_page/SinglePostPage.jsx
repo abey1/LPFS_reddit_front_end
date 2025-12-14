cf92760346a4dde5818ae0b31186f75285f725e0
abey <bruckabey@gmail.com>
2025-12-14 19:33:07 +1030

    data has been retrieved in single page

--- src/Pages/single_post_page/SinglePostPage.jsx
+++ src/Pages/single_post_page/SinglePostPage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import BackButton from "../../components/back_button/BackButton";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { singlePostSelector } from "./singePostPageSlice.js";
+import { useSelector } from "react-redux";
+import { fetchSingleRedditPost } from "./singePostPageSlice.js";
+
 const SinglePostPage = () => {
   const { sub, post_id } = useParams();
-
+  console.log("SinglePostPage params:", { sub, post_id });
+  const dispatch = useDispatch();
+  const { loading, error, post, comments } = useSelector(singlePostSelector);
+  useEffect(() => {
+    // Fetch the single post data using sub and post_id
+    // Example: dispatch(fetchSinglePost(sub, post_id));
+    dispatch(fetchSingleRedditPost({ sub, post_id }));
+  }, [sub, post_id]);
   return (
     <div>
       <BackButton />
--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navReducer from "./components/nav/navslice";
 import homeReducer from "./Pages/home_page/homePageSlice";
+import singlePageReducer from "./Pages/single_post_page/singePostPageSlice";
 
 const loggerMiddleware = () => (next) => (action) => {
-  // console.log("Before State:", store.getState());
+  console.log("Before State:", store.getState());
   let result = next(action);
-  // console.log("Next State:", store.getState());
+  console.log("Next State:", store.getState());
   return result;
 };
 
@@ -14,6 +15,7 @@ const store = configureStore({
     // Add your slices here
     nav: navReducer,
     homePage: homeReducer,
+    singlePost: singlePageReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(loggerMiddleware);
